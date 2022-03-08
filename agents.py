d57d7704d549bff691ee573e3de5489ffd484986
LouisDeOliveira <louisdeoliveira@gmail.com>
2022-03-08 23:06:02 +0100

    début des agents hihi

--- agents.py
+++ agents.py
@@ -1,34 +1,60 @@
 import numpy as np
+from constants import *
 from env import Env
 
 
 class Agent:
-
-    def __init__(self, pos: np.ndarray, vel: np.ndarray, acc: np.ndarray,
-                 env: Env):
+    def __init__(self,
+                 pos: np.ndarray = np.array([0., 0.]),
+                 vel: np.ndarray = np.array([0., 0.]),
+                 acc: np.ndarray = np.array([0., 0.]),
+                 env: Env = None) -> None:
 
         self.pos = pos
         self.vel = vel
         self.acc = acc
         self.env = env
 
+    def display(self,) -> None:
+        """        
+        Pygame operations to render the agent
+        """
 
-class Chercheur(Agent):
+        return None
 
+    def move(self, forces) -> None:
+
+        self.acc = forces
+        self.vel = self.vel + DT * self.acc
+        self.pos = self.pos + DT * self.vel
+
+
+class Seeker(Agent):
     def __init__(self,):
-        super(Chercheur, self).__init__()
+        super(Seeker, self).__init__()
+        self.status = {
+            "free": True,  # No target assigned
+            "going": False,  # Going to the target
+            "checking": False,  # Currently checking a target
+        }
 
 
-class Verificateur(Agent):
+class Checker(Agent):
     def __init__(self,):
-        super(Verificateur, self).__init__()
+        super(Checker, self).__init__()
 
 
 class Target(Agent):
     def __init__(self,):
         super(Target, self).__init__()
+        self.status = {
+            "targeted": False,  # Targeted by a seeker
+            "assigned": False,  # Assigned to checker
+            "checked": False,  # Cheked by checker
+        }
 
 
 if __name__ == '__main__':
-    chercheur = Chercheur()
-    print(chercheur)
+    seeker = Seeker()
+
+    print(seeker.pos)
